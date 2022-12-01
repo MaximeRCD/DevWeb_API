d311142c707e53bfb9b380b2781ba4708436340c
maxime richaudeau <maxime.richaudeau1@gmail.com>
2022-12-01 12:39:55 +0100

    Add reset password and delete user

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import databases
 import sqlalchemy
 
 # SQLAlchemy specific code, as with any other app
-DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/user_db"
+DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/garbage_app_db"
 
 database = databases.Database(DATABASE_URL)
 metadata = sqlalchemy.MetaData()
--- routers/users.py
+++ routers/users.py
@@ -11,17 +11,17 @@ user_router = APIRouter(
 
 
 class UserIn(BaseModel):
-    last_name: str
-    first_name: str
+    pseudo: str
     email: str
+    password: str
     last_updated: datetime.datetime = datetime.datetime.now()
 
 
 class User(BaseModel):
     id: int
-    last_name: str
-    first_name: str
+    pseudo: str
     email: str
+    password: str
     last_updated: datetime.datetime
 
 
@@ -29,9 +29,9 @@ users = sqlalchemy.Table(
     "users",
     metadata,
     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column("last_name", sqlalchemy.String(length=30)),
-    sqlalchemy.Column("first_name", sqlalchemy.String(length=30)),
+    sqlalchemy.Column("pseudo", sqlalchemy.String(length=30)),
     sqlalchemy.Column("email", sqlalchemy.String(length=100)),
+    sqlalchemy.Column("password", sqlalchemy.String(length=20)),
     sqlalchemy.Column("last_updated", sqlalchemy.TIMESTAMP)
 )
 
@@ -45,16 +45,41 @@ async def get_users():
 @user_router.post("/", tags=["users"], response_model=User)
 async def create_user(user_in: UserIn):
     query = users.insert().values(
-        last_name=user_in.last_name,
-        first_name=user_in.first_name,
+        pseudo=user_in.pseudo,
         email=user_in.email,
+        password=user_in.password,
         last_updated=user_in.last_updated
     )
-    insertion = await database.execute(query)
-    return {**user_in.dict(), "id": insertion}
+    try:
+        insertion = await database.execute(query)
+        return {**user_in.dict(), "id": insertion}
+    except Exception as e:
+        print(e)
+        return {**user_in.dict(), "id": 0}
 
 
-@user_router.get("/{username}", tags=["users"], response_model=User)
-async def read_user(username: str):
-    query = users.select().where(users.c.last_name == username)
+@user_router.get("/{pseudo}", tags=["users"], response_model=User)
+async def read_user(pseudo: str):
+    query = users.select().where(users.c.pseudo == pseudo)
     return await database.fetch_one(query)
+
+@user_router.put("/{pseudo}/reset_pwd", tags=["users"])
+async def reset_pwd(pseudo: str, pwd: str,  new_pwd: str):
+    query = users.update().where((users.c.pseudo == pseudo) & (users.c.password == pwd)).values(password=new_pwd)
+    reset_status =  await database.execute(query)
+    if reset_status == 1:
+        return {"message" : f"Password for user {pseudo} has been successfully set to {new_pwd}"}
+    else:
+        return {"message" : f" Wrong password given ! Can not modify it !"}
+
+@user_router.delete("/{pseudo}", tags=["users"])
+async def delete_user(pseudo: str, pwd: str):
+    query = users.delete().where((users.c.pseudo == pseudo) & (users.c.password == pwd))
+    # delete also all the data concerning the user in scan table
+    delete_status =  await database.execute(query)
+    if delete_status == 1:
+        return {"message" : f"User {pseudo} has been successfully deleted"}
+    else:
+        return {"message" : f" Wrong password given ! Can not delete this user !"}
+
+
